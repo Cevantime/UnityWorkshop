c519956b722f76e1f5f003b32f59a54c06774ed1
Thibault Truffert <thibaulttruffert@hotmail.com>
2021-01-28 13:27:25 +0100

    add ai nav

---
+++ Assets/Scripts/CharacterControl/NavigationMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityDungeon
+{
+    public class NavigationMovement : MonoBehaviour
+    {
+        private NavMeshAgent agent;
+        public new Camera camera;
+
+        void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
